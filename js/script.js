df0df4a6d87bfb5634619981710b27292fda8439
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-03-19 22:30:10 +0300

    introduce js script

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  let elements=[1,2]
+
+  elements.forEach((index)=>{
+    $('.content'+index).hover(function(){
+      $('.hidden-content'+index).toggle();
+      $('.content'+index).toggle()
+    });
+
+    $('.hidden-content'+index).hover(function(){
+      $('.content'+index).toggle();
+      $('.hidden-content'+index).toggle()
+    });
+  });
+
+})
