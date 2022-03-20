e0977d7bc4dd3bf522c97db436261fb71f2115c8
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-03-20 22:12:36 +0300

    hide additional buttons

--- js/script.js
+++ js/script.js
@@ -13,5 +13,12 @@ $(document).ready(function(){
     });
   });
 
-  
+
+  $("table").hide();
+  $(".additional-buttons").hide();
+  $(".additional-info").hide();
+  $(".btn.yes").hide();
+  $(".btn.no").hide();
+  $(".additional-info h4").hide();
+
 })
